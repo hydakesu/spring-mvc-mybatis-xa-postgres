c1b9982251a0e97bf98080fa3a6d13e44b88bd35
huang_yadong@ymsl.com.cn <120942963@qq.com>
2016-08-22 18:08:48 +0800

    edit

--- src/scripts/create.sql
+++ src/scripts/create.sql
@@ -6,10 +6,26 @@ CREATE TABLE rc1table
   CONSTRAINT config_key_1 PRIMARY KEY (id)
 )
 
+CREATE TABLE rc2table
+(
+  id bigint NOT NULL,
+  name character(45),
+  org character(10),
+  CONSTRAINT rc2table_key_1 PRIMARY KEY (id)
+)
+
 ##DB2
 CREATE TABLE demotable
 (
   id bigint NOT NULL,
   name character(45),
   CONSTRAINT userdemo_key_1 PRIMARY KEY (id)
+)
+
+CREATE TABLE demo1table
+(
+  id bigint NOT NULL,
+  name character(45),
+  org character(10),
+  CONSTRAINT demo1table_key_1 PRIMARY KEY (id)
 )
